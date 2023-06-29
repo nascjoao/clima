efaaf273f2df2599e996169892428ef948c95ff3
nascjoao <oi@joaonasc.dev>
2023-06-29 07:07:05 -0300

    refactor: move geolocation mock to jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,15 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+const mockGeolocation = {
+  getCurrentPosition: jest.fn()
+    .mockImplementation((success) => Promise.resolve(success({
+      coords: {
+        latitude: 42.12345,
+        longitude: -71.98765,
+      }
+    }))),
+};
+
+global.navigator.geolocation = mockGeolocation;
